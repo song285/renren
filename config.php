09aa37d378194317dff35992f22469e7ade0faf8
song285 <alen285@qq.com>
2019-12-08 12:39:51 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+	// 数据库主机地址
+	define('DB_HOST', 'localhost');
+	// 数据库用户名
+	define('DB_USER', 'root');
+	// 数据库密码
+	define('DB_PASSWORD', '123456');
+	// 数据库名字
+	define('DB_NAME', 'renren');
